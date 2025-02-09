4a772e9def7eb84582b0661dfcb3f8f8f7bc40f7
Luciano Elly <lucianoelly123@gmail.com>
2025-02-09 15:11:07 -0300

    layout app, fortify configuration

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,7 @@
 <?php
 
-use App\Models\User;
-use Illuminate\Mail\Message;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/email', function () {
-    
-    Mail::raw('Mensagem de teste de RH MANGNT', function (Message $message) {
-        $message->to('teste@gmail.com')
-                ->subject('Bem-vindo ao RH MANGNT')
-                ->from('rh@rhmangnt.com');
-    });
-
-    echo 'E-mail enviado com sucesso.';
-
+Route::middleware('auth')->group(function () {
+    Route::view('/home', 'home');
 });
-
-Route::get('/admin', function () {
-    $admin = User::with('detail', 'department')->find(1);
-    return view('admin', compact('admin'));
-});
-
-Route::view('/login', 'auth.login');
