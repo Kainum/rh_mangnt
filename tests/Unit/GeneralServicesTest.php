c881f4bdba683ba947b8332f0fca35a883e0e85b
Luciano Elly <lucianoelly123@gmail.com>
2025-02-25 13:44:47 -0300

    testes unitários

---
+++ tests/Unit/GeneralServicesTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Services\GeneralServices;
+
+it('tests if the salary is greater than a specific amount', function () {
+    $salary = 1000;
+    $amount = 500;
+
+    $result = GeneralServices::checkIfSalaryIsGreaterThan($salary, $amount);
+
+    expect($result)->toBeTrue();
+});
+
+it('tests if the salary is not greater than a specific amount', function () {
+    $salary = 1000;
+    $amount = 1500;
+
+    $result = GeneralServices::checkIfSalaryIsGreaterThan($salary, $amount);
+
+    expect($result)->toBeFalse();
+});
+
+it('tests if the phrase is created correctly', function () {
+    $name = 'Luciano';
+    $salary = 1500;
+
+    $result = GeneralServices::createPhraseWithNameAndSalary($name, $salary);
+
+    expect($result)->toBe('O salário do(a) Luciano é R$ 1500');
+});
+
+it('tests if the salary with bonus is calculated correctly', function () {
+    $salary = 1000;
+    $bonus = 25;
+
+    $result = GeneralServices::getSalaryWithBonus($salary, $bonus);
+
+    expect($result)->toBe(1025);
+});
